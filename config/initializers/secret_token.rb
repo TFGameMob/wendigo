1b3f5ca6d560670aeaad17da2f6995bfcbdf5c5d
Liss M <lissm@sdf.org>
2014-04-05 16:16:50 -0500

    Build rails and initialize Vagrant

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wendigo::Application.config.secret_key_base = 'f0213d2a35d46803b9584b6305131d9fb489abf34c705d5807764f5c18490ab0157e880278da284e49d8d1eb5b2f295270a7652bfa3aedad606168f65fccbde5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wendigo::Application.config.session_store :cookie_store, key: '_wendigo_session'
